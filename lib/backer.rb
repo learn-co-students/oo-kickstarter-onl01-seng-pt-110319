b84d19920f57a0fc08b4f1882319b5781c5fd1c5
arnie b <arnold.bwaila@gmail.com>
2020-01-09 01:05:29 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,11 +5,11 @@ class Backer
   def initialize(name)
     @name = name
     @backed_projects = []
-    @@all << self
   end
   
   def back_project(project)
     @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
   end
     
 end
